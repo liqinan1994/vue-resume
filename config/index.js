aee5cdec9f44a78806094b586b956e7c8bd88473
unknown <393820486@163.com>
2018-03-13 18:31:28 +0800

    修改路径2

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'vue-resume/dist',
+    assetsPublicPath: '/vue-resume/dist/',
 
     /**
      * Source Maps
